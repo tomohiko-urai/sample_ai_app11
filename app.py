077f76fb21f09edd8b7e46707127f6d32cb89b05
tomohiko-urai <84952494+tomohiko-urai@users.noreply.github.com>
2024-04-01 11:34:27 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -35,14 +35,14 @@ if img_file is not None:
         img = img.convert("RGB")
        # img = img.resize((image_size,image_size))
         
-        ####model = YOLO('last.pt')
-       #### ret = model(img,save=True, conf=0.6, iou=0.1)
-       #### annotated_frame = ret[0].plot(labels=True,conf=True)
-       #### annotated_frame = cv2.cvtColor(annotated_frame , cv2.COLOR_BGR2RGB)
+        model = YOLO('last.pt')
+        ret = model(img,save=True, conf=0.6, iou=0.1)
+        annotated_frame = ret[0].plot(labels=True,conf=True)
+        annotated_frame = cv2.cvtColor(annotated_frame , cv2.COLOR_BGR2RGB)
         
     
         # 結果の表示
         st.subheader("判定結果")
-       ##### st.image(annotated_frame, caption='出力画像') 
+        st.image(annotated_frame, caption='出力画像') 
         #st.write(camerapos[y] + "です。")
         #st.write(categories[y] + "です。")
