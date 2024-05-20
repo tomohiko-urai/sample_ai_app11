36052500fa03859076e93eef0a5cd437d74ac21c
tomohiko-urai <84952494+tomohiko-urai@users.noreply.github.com>
2024-05-20 21:30:13 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ col1,col2 = st.columns(2)
 with col1:    
     img_file = st.camera_input("カメラで撮影")
 
-with col2:
+#with col2:
 if img_file is not None:
     with st.spinner("推定中..."):
         img = Image.open(img_file)
