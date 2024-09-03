8be93c417ef88b4b1b61ab51479adceb16cb1861
tomohiko-urai <84952494+tomohiko-urai@users.noreply.github.com>
2024-09-03 09:50:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -24,10 +24,10 @@ col1,col2 = st.columns(2)
 img_source = st.sidebar.radio("画像のソースを選択してください。",
                               ("画像をアップロード", "カメラで撮影"))
 if img_source == "画像をアップロード":
-    with col1:
+    #with col1:
          img_file = st.sidebar.file_uploader("画像を選択してください。", type=["png", "jpg", "JPG"])
 elif img_source == "カメラで撮影":
-    with col1:    
+    #with col1:    
         img_file = st.camera_input("カメラで撮影")
 
 #with col2:
@@ -41,13 +41,13 @@ if img_file is not None:
        # img = img.resize((image_size,image_size))
         
         model = YOLO('last.pt')
-        ret = model(img,save=True, conf=0.4, iou=0.1)
+        ret = model(img,save=True, conf=0.2, iou=0.1)
         annotated_frame = ret[0].plot(labels=True,conf=True)
         annotated_frame = cv2.cvtColor(annotated_frame , cv2.COLOR_BGR2RGB)
         
     
         # 結果の表示
-    with col2:       
+    #with col2:       
         #st.subheader("判定結果")
         st.subheader("判定結果")
         st.image(annotated_frame, caption='出力画像', width=280) 
